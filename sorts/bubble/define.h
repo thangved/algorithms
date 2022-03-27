7fdfdb3f922f79e8700c39f180b25d96e9b722ee
thangved <thangcreation@gmail.com>
2022-03-27 13:50:31 +0700

    refactor

--- sorts/bubble/define.h
+++ sorts/bubble/define.h
@@ -18,9 +18,7 @@ void swap(Recordtype &a, Recordtype &b)
 void bubblesort(vector<Recordtype> &data)
 {
     for (int i = (int)data.size(); i > 0; i--)
-    {
         for (int j = 0; j < i - 1; j++)
             if (data[j].key > data[j + 1].key)
                 swap(data[j], data[j + 1]);
-    }
 }
